9b2b88e452142e2498fbdc6b3c26482b1a39fe94
Oscar Grainger <oscarvgrainger@gmail.com>
2017-12-01 15:01:57 +0100

    fix: tests

--- controllers/articlelist-controller.js
+++ controllers/articlelist-controller.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 const BadRequestError = require('../errors/bad-request');
 const generator = require('../utils/uuid-generator');
 const {find} = require('lodash');
